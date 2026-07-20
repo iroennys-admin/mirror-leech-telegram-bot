676122b57f29de0e48e8c7701834f94ce07a7999
iroennys-admin <iroennys-admin@example.com>
2026-07-20 04:35:35 -0400

    use built-in web server (BASE_URL_PORT=8000) for Koyeb healthcheck

--- healthcheck.py
+++
@@ -1,8 +0,0 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-class H(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'ok')
-    def log_message(self, *a): pass
-HTTPServer(('', 8000), H).serve_forever()
